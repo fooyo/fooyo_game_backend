8e4ca2440384be514cd6725401461c7ab843dcea
yfscret <305667862@qq.com>
2022-12-26 21:01:48 +0800

    fx admin cards

--- app/admin/cards.rb
+++ app/admin/cards.rb
@@ -2,7 +2,13 @@ ActiveAdmin.register Card do
   includes :user
   menu priority: 1
   config.batch_actions = false
-  config.sort_order = 'is_used_desc'
+  config.sort_order = 'customer_order_asc'
+  # config.paginate = true
+  # config.per_page = 20
+  # actions :all, except: %i[new destroy]
+  order_by(:customer_order) do |order_clause|
+    ['user_id is null', 'is_used desc'].join(", ")
+  end
    # config.paginate = true
   # config.per_page = 20
   actions :all, except: %i[new destroy edit show]
@@ -10,6 +16,9 @@ ActiveAdmin.register Card do
   index download_links: false do
     column :code
     column :card_type_text
+    column '是否使用' do |resource|
+      resource.user_id.present?
+    end
     column :is_used
     column :user do |resource|
       simple_format "姓名: #{resource.user&.real_name},  电话: #{resource.user&.mobile},  email: #{resource.user&.email}" if resource.user
